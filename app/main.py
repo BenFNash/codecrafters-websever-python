db53b69098850e22368837d85ff53f6ea4b95528
Ben Nash <benfnash@gmail.com>
2024-05-29 07:34:25 +0100

    user-agent

--- app/main.py
+++ app/main.py
@@ -14,6 +14,17 @@ class RequestHandler(BaseHTTPRequestHandler):
             response += f"Content-Length: {len(path_chunks[2])}\r\n\r\n"
             response += path_chunks[2]
             response = bytes(response, 'utf-8')
+        elif path_chunks[1] == "user-agent":
+            user_agent = self.headers.get("User-Agent")
+            if user_agent:
+                response = "HTTP/1.1 200 OK\r\n\r\n"
+                response += "Content-Type: text/plain\r\n"
+                response += f"Content-Length: {len(user_agent)}\r\n\r\n"
+                response += user_agent
+                response = bytes(response, 'utf-8')
+            else:
+                response = b"HTTP/1.1 400 Bad Request\r\n\r\n"
+
 
         else:
             response = b"HTTP/1.1 404 Not Found\r\n\r\n"
@@ -23,30 +34,6 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         ...
 
-
-
-# def handle_client(client_socket, address):
-#     print(f"Accepted connection from {address}")
-#     request = client_socket.recv(1024).decode("utf-8")
-#
-#     path = request.split(" ")[1]
-#     if path == "/":
-#         response = "HTTP/1.1 200 OK\r\n\r\n"
-#     else:
-#         response = "HTTP/1.1 404 Not Found\r\n\r\n"
-#     
-#     client_socket.send(response.encode())
-#     client_socket.close()
-
-
-# def start_server(host, port):
-#     server = socket.create_server((host, port), reuse_port=True)
-#     server.listen(1)
-#     print(f"Server listening on {host}:{port}")
-#
-#     while True:
-#         client_socket, address = server.accept()
-#         handle_client(client_socket, address)
 
 def start_server(host, port):
     with socketserver.TCPServer((host, port), RequestHandler, bind_and_activate=False) as server:
